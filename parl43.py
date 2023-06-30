c220e4f3be2092f5bd3ef00257954c8ce3db3b71
Robert W. McBean <mcbeanit@users.noreply.github.com>
2023-06-29 18:57:10 -0600

    43rd parliament get members list

---
+++ parl43.py
@@ -0,0 +1,14 @@
+import re
+
+regex = r'<tr>.+?<\/tr>'
+
+matches = re.finditer(regex, test_str, re.MULTILINE)
+
+for matchNum, match in enumerate(matches, start=1):
+    
+    print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
+    
+    for groupNum in range(0, len(match.groups())):
+        groupNum = groupNum + 1
+        
+        print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
